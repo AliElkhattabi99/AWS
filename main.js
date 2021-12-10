8620a2800689778e423ae2a73d54886969eb6030
Ali <s116100@ap.be>
2021-12-10 20:49:30 +0100

    cognito1.0

---
+++ main.js
@@ -0,0 +1,5 @@
+const { registerUser, login, validateToken } = require("./security");
+
+function log(em, pw) {
+  cog.registerUser("s116100@ap.be", "Ali.2580");
+}
--- s3.js
+++ s3.js
@@ -1,10 +1,22 @@
 require("dotenv").config();
 const express = require("express");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var path = require("path");
 const app = express();
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 const aws = require("aws-sdk");
-const cog = require("./cog");
+const { registerUser, login, validateToken } = require("./security");
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const s3 = new aws.S3({});
 
@@ -37,19 +49,73 @@ con.connect(function (err) {
   con.query(
     "CREATE TABLE IF NOT EXISTS Uid(ID int NOT NULL AUTO_INCREMENT, Url VARCHAR(50), PRIMARY KEY(ID) )"
   );
+  con.query(
+    "CREATE TABLE IF NOT EXISTS accounts ( id int(11) NOT NULL, password varchar(255) NOT NULL, email varchar(100) NOT NULL) ENGINE=InnoDB"
+  );
   console.log("Connected!");
 });
 
-/*app.get("/public/home", function (req, res) {
-  res.render(__dirname + "/public/home.html"); //if html file is within public directory
-});*/
-app.get("/", function (req, res) {
+app.post("/reg", function (req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+  registerUser(email, password);
   res.sendFile(__dirname + "/public/home.html");
 });
 
-/*app.get("/public/home.html", function (req, res) {
-  res.sendFile("/public/home.html");
+/*app.post("/reg", function (req, res, next) {
+  inputData = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  var sql = "SELECT * FROM accounts WHERE email =?";
+  con.query(sql, [inputData.email], function (err, data, fields) {
+    if (err) throw err;
+    if (data.length > 1) {
+      var msg = inputData.email + "was already exist";
+    } else {
+      // save users data into database
+      var sql = "INSERT INTO accounts SET ?";
+      con.query(sql, inputData, function (err, data) {
+        if (err) throw err;
+      });
+      var msg = "Your are successfully registered";
+    }
+    res.sendFile(__dirname + "/public/home.html");
+  });
+});
+
+app.post("/auth", function (request, response) {
+  var username = request.body.email;
+  var password = request.body.password;
+  if (username && password) {
+    con.query(
+      "SELECT * FROM accounts WHERE email = ? AND password = ?",
+      [username, password],
+      function (error, results, fields) {
+        if (results.length > 0) {
+          request.session.loggedin = true;
+          request.session.username = username;
+          response.redirect("/home");
+        } else {
+          response.send("Incorrect Username and/or Password!");
+        }
+        response.sendFile(__dirname + "/public/home.html");
+      }
+    );
+  } else {
+    response.send("Please enter Username and Password!");
+    response.end();
+  }
 });*/
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/link.html"));
+});
+
+app.get("/home", function (req, res) {
+  res.sendFile(__dirname + "/public/home.html");
+});
 
 app.post("/save-image", upload.single("image"), (req, res) => {
   res.redirect(req.file.location);
@@ -57,7 +123,7 @@ app.post("/save-image", upload.single("image"), (req, res) => {
   con.query("INSERT INTO Uid(Url) VALUES('" + req.file.location + "')");
 });
 app.get("/ali", function (req, res) {
-  con.query("SELECT Url FROM Uid WHERE ID=10", function (err, ress, fields) {
+  con.query("SELECT Url FROM Uid WHERE ID=11", function (err, ress, fields) {
     if (err) throw err;
     image = ress[0].Url;
 
